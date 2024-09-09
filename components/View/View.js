11a955fb242452e48cc5827ce39f2c2fe54ec4eb
Doomcaster1917 <Webtalestoday@gmail.com>
2024-09-09 19:46:46 +0300

    Added styles to view(users) page

--- components/View/View.js
+++ components/View/View.js
@@ -3,11 +3,15 @@ import styles from './View.module.scss';
 
 const View = ({children}) => {
     return (
-        <div>
+        <div className={styles.main_wrapper}>
             <h1>{children.Title}</h1>
+            <h2>{children.Description}</h2>
             <div className={styles.charts_area}>
                 {children.BoundedCharts.map((chart, index) => (
-                    <iframe srcDoc={chart.HtmlChart} frameBorder="0"></iframe>
+                    <>
+                        <iframe key={index} srcDoc={chart.HtmlChart} frameBorder="0"></iframe>
+                        <p>{chart.Description}</p>
+                    </>
                 ))}
             </div>
         </div>
