40c2ca1e746eba78915fd21958c7fe67c037d700
Doomcaster1917 <Webtalestoday@gmail.com>
2024-09-17 18:40:11 +0300

    Added facivon.ico

--- components/MainContainer/MainContainer.js
+++ components/MainContainer/MainContainer.js
@@ -12,7 +12,7 @@ const MainContainer = ({children, title, description, keywords}) => {
                 <title>{title}</title>
                 <meta name={"description"} content={description}/>
                 <meta name={"keywords"} content={keywords}/>
-                <link rel="icon" type="image/x-icon" href="../static/images/logo120.svg"/>
+                <link rel="icon" type="image/x-icon" href="../static/images/favicon.ico"/>
                 <meta name="yandex-verification" content="fd324c95d81fb05d"/>
             </Head>
             <Suspense>
